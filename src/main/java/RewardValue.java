ca48cc074d9b06026c1178702a43f3f00ded4a3e
romani006 <romaninanli18@gmail.com>
2024-09-07 13:58:59 +0100

    Fixed the problem and completed the task.

---
+++ src/main/java/RewardValue.java
@@ -0,0 +1,19 @@
+public class RewardValue {
+    
+    double cashValue;
+
+    public RewardValue(double cashValue) {
+        this.cashValue = cashValue;
+    }
+
+    public double getCashValue(){
+        return cashValue;
+    }
+
+    public double getMilesValue(){
+        return cashValue * 0.0035;
+    }
+
+
+}
+
